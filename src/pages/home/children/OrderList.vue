a8f2ffc7c39e0c52a792db71d44d48d39199a9f4
F-xd <1178598275@qq.com>
2025-04-14 23:47:34 +0800

    订单列表

--- src/pages/home/children/OrderList.vue
+++ src/pages/home/children/OrderList.vue
@@ -22,7 +22,7 @@
           end-placeholder="结束日期" :shortcuts="shortcuts" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm()">
+        <el-button type="primary" @click="submitForm(ruleFormRef)">
           搜索
         </el-button>
         <el-button @click="resetForm(ruleFormRef)">重置</el-button>
@@ -31,7 +31,7 @@
     
 
     <div class="container">
-      <el-table :data="tableData.data" row-key="id" style="width: 100%">
+      <el-table :row-style="(a)=>{return {backgroundColor:a.rowIndex%2==0?'#eee8aa':'#87ceeb'}}" :data="tableData.data" row-key="id" style="width: 100%">
         <el-table-column property="orderNo" label="订单号" width="70"></el-table-column>
         <el-table-column property="orderTime" label="下单时间"></el-table-column>
         <el-table-column property="phone" label="联系电话"></el-table-column>
@@ -43,9 +43,9 @@
         <el-table-column property="orderState" label="订单状态"></el-table-column>
         <el-table-column label="操作">
           <template #default="scope">
+            <el-button size="small" type="danger" @click="handleEdit(scope.row.id,true)">查看</el-button>
             <el-button size="small" type="primary"
-              @click="handleEdit(scope.row.id, scope.row.account, scope.row.userGroup)">编辑</el-button>
-            <el-button size="small" type="danger" @click="handleDelete(scope.row.id)">删除</el-button>
+              @click="handleEdit(scope.row.id)">编辑</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -53,13 +53,73 @@
         layout="total, sizes, prev, pager, next, jumper" :total="tableData.total" />
     </div>
 
+    <!-- 弹窗 -->
+    <el-dialog v-model="dialogFormVisible" draggable title="修改订单信息" width="500px">
+      <el-form :disabled="isView" label-width="100" :model="form">
+        <el-form-item label="订单号">
+          <el-input v-model="form2.orderNo" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="下单时间">
+          <el-date-picker
+            v-model="form2.orderTime"
+            type="datetime"
+          />
+        </el-form-item>
+        <el-form-item label="联系电话">
+          <el-input v-model="form2.phone" />
+        </el-form-item>
+        <el-form-item label="收货人">
+          <el-input v-model="form2.consignee" />
+        </el-form-item>
+        <el-form-item label="送货地址">
+          <el-input v-model="form2.deliverAddress" />
+        </el-form-item>
+        <el-form-item label="送达时间">
+          <el-date-picker
+            v-model="form2.deliveryTime"
+            type="datetime"
+          />
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input v-model="form2.remarks"/>
+        </el-form-item>
+        <el-form-item label="订单金额">
+          <el-input-number :precision="2" :min="0"  v-model="form2.orderAmount" />
+        </el-form-item>
+        <el-form-item label="订单状态">
+          <el-select v-model="form2.orderState">
+            <el-option label="已受理" value="已受理" />
+            <el-option label="派送中" value="派送中" />
+            <el-option label="已完成" value="已完成" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">{{ isView ? '关闭' : '取消' }}</el-button>
+          <el-button v-show="!isView" type="primary" @click="async ()=>{
+            const {data} = await axios.post('/order/edit', form2);
+            if(data.code === 0){
+              ElMessage.success('修改成功');
+              await getData(form);
+              dialogFormVisible = false;
+            }else{
+              ElMessage.error('请检查输入是否正确');
+            }
+          }">确认</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script setup>
 import { ref, reactive,watch } from 'vue';
 import axios from '@axios';
+import { ElMessage } from 'element-plus'
+const isView = ref(false);
 const ruleFormRef = ref();
+const dialogFormVisible = ref(false);
 // 数据列表
 const tableData = reactive({});
 // 表单数据
@@ -72,6 +132,20 @@ const form = reactive({
   currentPage: 1,
   pageSize: 5
 })
+// 订单修改表单
+const form2 = reactive({
+  id: '',
+  orderNo: '',
+  orderTime: '',
+  phone:'',
+  consignee: '',
+  deliverAddress: '',
+  deliveryTime: '',
+  remarks: '',
+  orderAmount: '',
+  orderState: ''
+});
+
 // 日期选择器快捷选项
 const shortcuts = [
   {
@@ -113,11 +187,8 @@ const shortcuts = [
 ]
 // 获取数据
 const getData = async (form) => {
-  console.log(form);
-  form.date = JSON.stringify(form.date);
-  console.log(form);
+  form.date = form.date?JSON.stringify(form.date):'';
   const { data } = await axios.get('/order/list?' + new URLSearchParams(form));
-  console.log(data);
   tableData.data = data.data;
   tableData.total = data.total;
 }
@@ -128,14 +199,22 @@ watch(() => ([form.currentPage, form.pageSize]), () => {
 
 
 // 根据表单数据获取数据
-const submitForm = () => {
+const submitForm = (ruleFormRef) => {
   getData(form);
+  form.date = '';
 }
 
+// 编辑订单
+const handleEdit = async (id,viewed=false) => {
+  Object.assign(form2, tableData.data.find(item => item.id == id));
+  isView.value = viewed;
+  dialogFormVisible.value = true;
+}
 
 // 表单重置
 const resetForm = (ruleFormRef) => {
   ruleFormRef.resetFields();
+  getData(form);
 }
 </script>
 
--- src/pages/home/children/ShopInfo.vue
+++ src/pages/home/children/ShopInfo.vue
@@ -1,15 +1,132 @@
 <template>
   <div class='shopInfo'>
-    <h1>shopInfo</h1>
+    <Title title="账号列表">
+      <el-button type="primary" size="large" @click="()=>{handleEdit('','', false,true)}">保存店铺信息</el-button>
+    </Title>
+    <div class="container">
+      <el-form ref="ruleFormRef" :model="form" label-width="auto" class="demo-ruleForm">
+        <el-form-item label="店铺名称"  prop="name">
+          <el-input v-model="form.name" type="text" />
+        </el-form-item>
+        <el-form-item label="店铺公告" prop="bulletin">
+          <el-input v-model="form.bulletin" type="textarea" />
+        </el-form-item>
+        <el-form-item label="店铺头像" prop="avatar">
+          <el-upload
+            class="avatar-uploader"
+            :action="axios.defaults.baseURL + '/shop/upload'"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+          >
+            <img v-if="form.avatar" :src="axios.defaults.baseURL + form.avatar" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon" color="#8c939d" size="40"><Plus/></el-icon>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="起送价格" prop="deliveryPrice">
+          <el-input v-model="form.deliveryPrice" type="number" placeholder="请输入起送价格" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="送达时间" prop="deliveryTime">
+          <el-input v-model="form.deliveryTime" type="text" placeholder="请输入送达时间" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="配送描述" prop="description">
+          <el-input v-model="form.description" type="textarea" placeholder="请输入配送描述" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="店铺好评率" prop="score">
+          <el-input v-model="form.score" type="number" placeholder="请输入店铺好评率" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="店铺销量" prop="sellCount">
+          <el-input v-model="form.sellCount" type="number" placeholder="请输入店铺销量" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="活动支持" prop="sellCount">
+          <el-checkbox-group v-model="form.supports">
+            <el-checkbox label="单人精彩套餐" value="单人精彩套餐" />
+            <el-checkbox label="VC无限橙果汁全场8折" value="VC无限橙果汁全场8折" />
+            <el-checkbox label="在线支付满28减5" value="在线支付满28减5" />
+            <el-checkbox label="特价饮品八折抢购" value="特价饮品八折抢购" />
+            <el-checkbox label="中秋特惠" value="中秋特惠" />
+            <el-checkbox label="国庆特价" value="国庆特价" />
+            <el-checkbox label="春节1折折扣" value="春节1折折扣" />
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item label="营业时间" prop="date">
+          <el-time-picker  v-model="form.date" is-range
+          arrow-control range-separator="到" start-placeholder="开始时间"
+            end-placeholder="结束时间" />
+        </el-form-item>
+      </el-form>
+
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, reactive } from 'vue';
+import Title from '../../../components/Title.vue';
+import axios from '@axios';
+import { Plus } from '@element-plus/icons-vue'
+
+const form = reactive({
+  id:'',
+  name:'',
+  bulletin:'',
+  avatar:'',
+  deliveryPrice:'',
+  minPrice:'',
+  deliveryTime:'',
+  description:'',
+  score:'',
+  sellCount:'',
+  supports:[],
+  date:[],
+  pics:[]
+})
+
+
+// 获取数据
+const getData = async () => {
+  const { data:{data} } = await axios.get('/shop/info');
+  Object.assign(form, data);
+  form.supports = [];
+  form.date = [];
+}
+getData();
+
+// 上传图片成功回调
+const handleAvatarSuccess = (res, file, fileList) => {
+  form.avatar = res.imgUrl;
+  console.log(form);
+}
+
+
+// 保存店铺信息
+const handleEdit = async(from)=>{
+  const {data} = await axios.post('/shop/edit', form);
+  console.log(data);
+}
 </script>
 
 <style lang='less' scoped>
-.shopInfo {
+.container{
+  padding: 15px;
+}
+.avatar-uploader {
+  width: 200px;
+  height: 200px;
+  border: 2px dashed #d9d9d9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
 
+.avatar-uploader:hover {
+  border-color: #409EFF;
+}
+.avatar {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* 保持图片的宽高比并覆盖整个容器 */
 }
 </style>
