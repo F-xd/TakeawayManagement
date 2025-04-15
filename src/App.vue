1939b69eba3e24f75e65663a852a6a27e135e604
F-xd <1178598275@qq.com>
2025-04-15 22:32:05 +0800

    fix:修改配置

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,14 @@ router.push('/home');
 router.beforeEach(async (to, from, next) => {
   if(to.path !== '/login'){
     try{
-      // const {data} = await axios.get('/users/checktoken?token='+localStorage.getItem('token'));
-      // if(data.code !== 0){
-      //   throw new Error('token失效');
-      // }
-      // else{
+      const {data} = await axios.get('/users/checktoken?token='+localStorage.getItem('token'));
+      if(data.code !== 0){
+        throw new Error('token失效');
+      }
+      else{
         const {data} = await axios.get('/users/accountinfo?id='+localStorage.getItem('id'));
         useAccountInfoStore().setAccountInfo(data.accountInfo);
-      // }
+      }
     }
     catch(e){
       console.error(e);
