bb280c5714fea8c75e843300f57998437fac7ac8
F-xd <1178598275@qq.com>
2025-04-15 23:12:52 +0800

    feat:切换baseURL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 const token = localStorage.getItem('token')
 
 const instance = axios.create({
-  baseURL: 'http://8.137.157.16:9002',
-  // baseURL:'/api',
+  // baseURL: 'http://8.137.157.16:9002',
+  baseURL:'/api',
   timeout: 3000,
   headers: {
     Authorization: `Bearer ${token}`
